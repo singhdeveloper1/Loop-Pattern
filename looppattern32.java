33f44abcf49dd12594ef9e72b105bd12dbf01909
Priyanshu <118466432+Chauhan-coder@users.noreply.github.com>
2023-02-08 23:48:58 +0530

    Add files via upload

---
+++ looppattern32.java
@@ -0,0 +1,27 @@
+public class looppattern32 {
+    public static void main(String[] args) {
+        // 1
+        // 1*2
+        // 1*2*3
+        // 1*2*3*4
+        // 1*2*3*4*5
+        // 1*2*3*4
+        // 1*2*3
+        // 1*2
+        // 1
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + "*");
+            }
+            System.out.println();
+        }
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + "*");
+            }
+            System.out.println();
+        }
+    }
+
+}
