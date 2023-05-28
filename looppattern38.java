c20bb70320a0d8810e5c1874ff8a472292a99752
Priyanshu Singh <118466432+singhdeveloper1@users.noreply.github.com>
2023-05-28 22:58:54 +0530

    Add files via upload

---
+++ looppattern38.java
@@ -0,0 +1,27 @@
+public class looppattern38 {
+
+//      *****
+//     ***** 
+//    *****
+//   *****
+//  *****
+
+    public static void main(String[] args) {
+        
+        for(int i=0;i<5;i++){
+            
+                for(int k=4;k>i;k--){
+                System.out.print(" ");
+                }
+                
+                    for(int j=0;j<=4;j++)
+{
+    System.out.print("*");
+}
+System.out.println();
+                }
+            
+
+        }
+    
+    }
